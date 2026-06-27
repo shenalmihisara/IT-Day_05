b5914c5f32f48a29436b1ccb17e51eefd40bd9d3
shenalmihisara <shenalmihisara51@gmail.com>
2026-06-27 15:23:24 +0530

    Comment out variable declarations and switch statement in app.js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let salary = 7500.0;
 
 console.log(typeof name);*/
 
-let x = 101;
+/*let x = 101;
 let y = "101";
 
 console.log(x===y);
@@ -25,6 +25,30 @@ if (false | false){
     console.log(false);
 }
 
-console.log(!false);
+console.log(!false);*/
 
+/*let day = 2;
 
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break
+    case 3:
+        console.log("Wednesday");
+        break
+    case 4:
+        console.log("Thursday");
+        break
+    default:
+        console.log("invalid day");
+}*/
+
+function add(x, y){
+    return x + y;
+}
+
+let results = add(10, 20);
+console.log(results);
