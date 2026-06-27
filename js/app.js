a8a8e0176ce8296fc051b92de5a1ee70cb47f5c9
shenalmihisara <shenalmihisara51@gmail.com>
2026-06-27 14:30:11 +0530

    Initialize variables and log name type in app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
-console.log("Hello java Script :)");
-console.log("Shenal")
+let name = "Saman";
+let age = 18;
+let isAfterAL = false;
+let coustomers = [];
+let costomer = {};
+let salary = 7500.0;
+
+
+console.log(typeof name);
+
+
+
+
