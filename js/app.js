48eba4c675c44ca5d6eea066c8e6af6a600fc5e1
shenalmihisara <shenalmihisara51@gmail.com>
2026-06-27 14:53:05 +0530

    Comment out initial variable declarations and add type comparison and logical operation examples

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let name = "Saman";
+/*let name = "Saman";
 let age = 18;
 let isAfterAL = false;
 let coustomers = [];
@@ -6,8 +6,25 @@ let costomer = {};
 let salary = 7500.0;
 
 
-console.log(typeof name);
+console.log(typeof name);*/
 
+let x = 101;
+let y = "101";
 
+console.log(x===y);
+
+if(true & false){
+    console.log(true);
+}else{
+    console.log(false)
+}
+
+if (false | false){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+console.log(!false);
 
 
